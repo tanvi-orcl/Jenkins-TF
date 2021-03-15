c4ac064ed28c74f8244b9b0dacfb0a99e64ebf84
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-15 10:49:19 -0500

    added back a credentials

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider oci {
 	region 				 = var.primary_region
-	# tenancy_ocid         = var.tenancy_id   
+	tenancy_ocid         = var.tenancy_id   
   	# user_ocid            = var.user_id
   	# fingerprint          = var.api_fingerprint
   	# private_key_path     = var.api_private_key_path
