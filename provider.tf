8b6eb8278508c9003b5ed8cc4bccf514e179dafe
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-15 10:52:40 -0500

    fixed credentials

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider oci {
 	region 				 = var.primary_region
 	tenancy_ocid         = var.tenancy_id   
-  	# user_ocid            = var.user_id
-  	# fingerprint          = var.api_fingerprint
-  	# private_key_path     = var.api_private_key_path
+  	user_ocid            = var.user_id
+  	fingerprint          = var.api_fingerprint
+  	private_key_path     = var.api_private_key_path
 }
 
 # provider oci { 
--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable primary_region { default = "us-sanjose-1" }
 # variable region { default = "eu-frankfurt-1" }
 
 variable api_fingerprint { default = "3f:c2:66:ad:6b:95:9e:d4:2a:3b:a6:97:89:6d:bb:6e" }
-variable api_private_key_path { default =  "/Users/tvaradha/.ssh/terraform_api_key.pem" }
+variable api_private_key_path { default =  "/home/opc/jenkins_dir/api_key.pem" }
 variable tenancy_id { default =  "ocid1.tenancy.oc1..aaaaaaaaplkmid2untpzjcxrmbv4nowe74yb4lr6idtckwo4wyf7jh23be4q" }
 variable user_id { default =  "ocid1.user.oc1..aaaaaaaa5in22b5bvkncp373g2mkhi6vh2cspj4vt2j5sx27576pql75umda" }
 
