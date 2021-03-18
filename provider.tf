cf3c56f016fceb5838be99d16d9515137a97240e
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-18 17:10:18 -0500

    fixed variable name

--- provider.tf
+++ provider.tf
@@ -10,14 +10,5 @@ provider oci {
 	tenancy_ocid         = var.tenancy_id   
   	user_ocid            = var.user_id
   	fingerprint          = var.api_fingerprint
-	private_key_path     = var.api_key
+	private_key_path     = var.api_private_key_path
 }
-
-# provider oci { 
-#   	alias                = "sanjose"
-#   	region               = var.primary_region
-#   	tenancy_ocid         = var.tenancy_id
-# 	user_ocid            = var.user_id
-#   	fingerprint          = var.api_fingerprint
-#   	private_key_path     = var.api_private_key_path
-# }
