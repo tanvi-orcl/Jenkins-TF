c82777f726d36e569f9a02e5ea0d93c9846d3524
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-18 11:02:32 -0500

    change environment variable format

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider oci {
   	user_ocid            = var.user_id
   	fingerprint          = var.api_fingerprint
   	#private_key_path     = var.api_private_key_path
-	private_key_path     = env.TF_API_KEY
+	private_key_path     = var.TF_API_KEY
 }
 
 # provider oci { 
