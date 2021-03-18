9526011e719f121f0da04a68ebc4f8c723a7cc55
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-18 11:06:48 -0500

    change environment variable format

--- vars.tf
+++ vars.tf
@@ -3,9 +3,10 @@ variable primary_region { default = "us-sanjose-1" }
 variable region { default = "eu-frankfurt-1" }
 
 variable api_fingerprint { default = "3f:c2:66:ad:6b:95:9e:d4:2a:3b:a6:97:89:6d:bb:6e" }
-variable api_private_key_path { default =  "/var/lib/jenkins/workspace/tf_api_key.pem" }
+variable api_private_key_path { default =  "/var/lib/jenkins/workspace/api_key.pem" }
 variable tenancy_id { default =  "ocid1.tenancy.oc1..aaaaaaaaplkmid2untpzjcxrmbv4nowe74yb4lr6idtckwo4wyf7jh23be4q" }
 variable user_id { default =  "ocid1.user.oc1..aaaaaaaa5in22b5bvkncp373g2mkhi6vh2cspj4vt2j5sx27576pql75umda" }
 
+variable terraform_api_path {}
 # variable export_terraform_test_source_image_id { default = "ocid1.image.oc1.us-sanjose-1.aaaaaaaag2fam5xawz7t3ad5u3mzxdhglxeldohlijdsjfaielqluysrc3ga" }
 
