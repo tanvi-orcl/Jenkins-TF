7c1d236ed06fdf0fb34a7c3297ef788a29363bd7
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-03-18 10:07:24 -0500

    secret file

--- availability_domain.tf
+++ availability_domain.tf
@@ -5,12 +5,12 @@ data oci_identity_availability_domain rLid-SANJOSE-1-AD-1 {
   ad_number      = "1"
 }
 
-data oci_identity_availability_domain rLid-SANJOSE-1-AD-2 {
-  compartment_id = var.compartment_ocid
-  ad_number      = "2"
-}
+# data oci_identity_availability_domain rLid-SANJOSE-1-AD-2 {
+#   compartment_id = var.compartment_ocid
+#   ad_number      = "2"
+# }
 
-data oci_identity_availability_domain rLid-SANJOSE-1-AD-3 {
-  compartment_id = var.compartment_ocid
-  ad_number      = "3"
-}
+# data oci_identity_availability_domain rLid-SANJOSE-1-AD-3 {
+#   compartment_id = var.compartment_ocid
+#   ad_number      = "3"
+# }
